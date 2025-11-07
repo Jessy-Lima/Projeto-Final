e8ac53bcd952a6cd95f4b8892b9eec2de9e5efb0
Jessica <maytajessica73@gmail.com>
2025-11-07 16:54:05 -0300

    adicionando o post no main

--- Back-end/main.py
+++ Back-end/main.py
@@ -7,3 +7,9 @@ app = FastAPI(title="Gerenciador de produtos")
 def home():
     return {"mensagem": "Bem-Vindo ao gerenciador de produtos"}
 
+
+@app.post("/produtos")
+def cadastrar_produto(nome: str, categoria: str, preco: float, quantidade: int):
+    funcao.adicionar_produto(nome, categoria, preco, quantidade)
+    return {"mensagem": "Produto cadastrado com sucesso!"}
+
--- Front-end/app.py
+++ Front-end/app.py
@@ -1,3 +1,24 @@
 import streamlit as st
 import requests 
 
+API_URL = "http://10.104.82.31:8501"
+
+st.set_page_config(page_title="Produtos", layout="wide")
+
+st.title("Gerenciador de Produtos")
+
+menu = st.sidebar.radio("Menu", 
+    ["Catalogo", "Cadastrar Produto", "Deletar Produto", "Atualizar quantidade","Estoque"]
+    )
+if menu == "Catalogo":
+    st.subheader("Todos os Produtos")
+    response = requests.get(f"{API_URL}/produtos")
+    if response.status_code == 200:
+        produtos = response.json().get("produtos", [])
+        if produtos:
+            st.dataframe(produtos)
+        else:
+            st.info("Nenhum produto cadastrado ainda!")
+    else:
+        st.error("Erro ao conectar com a API.")
+        
